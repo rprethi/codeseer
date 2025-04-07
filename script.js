e38936bb898facf226b517282e7d0c2fff8c1807
rprethi <2271821@cmontmorency.qc.ca>
2025-04-07 15:15:14 -0400

    init

---
+++ script.js
@@ -0,0 +1,26 @@
+const swiper = new Swiper(".role-swiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+      delay: 10000, // 10 secondes
+      disableOnInteraction: false, // continue même si l'utilisateur interagit
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+  
